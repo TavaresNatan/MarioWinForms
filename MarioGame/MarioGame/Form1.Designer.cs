a35503089cb4584cdc321f59565e848ac2bd0702
TavaresNatan <99446721+TavaresNatan@users.noreply.github.com>
2022-10-03 11:12:29 -0300

    power up added
    bigger jump

--- MarioGame/MarioGame/Form1.Designer.cs
+++ MarioGame/MarioGame/Form1.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.components = new System.ComponentModel.Container();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.buy_panel = new System.Windows.Forms.Panel();
+            this.higher_jumpbtn = new System.Windows.Forms.Button();
             this.jump_btn = new System.Windows.Forms.Button();
             this.Mario = new System.Windows.Forms.PictureBox();
             this.Flower = new System.Windows.Forms.PictureBox();
@@ -42,7 +44,9 @@
             this.buy_points = new System.Windows.Forms.Label();
             this.bp = new System.Windows.Forms.Label();
             this.pause = new System.Windows.Forms.Label();
+            this.startbtn = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
+            this.buy_panel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.Mario)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.Flower)).BeginInit();
             this.SuspendLayout();
@@ -50,6 +54,7 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.SeaGreen;
+            this.panel1.Controls.Add(this.buy_panel);
             this.panel1.Controls.Add(this.jump_btn);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
             this.panel1.Location = new System.Drawing.Point(0, 397);
@@ -57,17 +62,44 @@
             this.panel1.Size = new System.Drawing.Size(800, 53);
             this.panel1.TabIndex = 0;
             // 
+            // buy_panel
+            // 
+            this.buy_panel.BackColor = System.Drawing.Color.Honeydew;
+            this.buy_panel.Controls.Add(this.higher_jumpbtn);
+            this.buy_panel.Location = new System.Drawing.Point(10, 8);
+            this.buy_panel.Name = "buy_panel";
+            this.buy_panel.Size = new System.Drawing.Size(427, 40);
+            this.buy_panel.TabIndex = 1;
+            // 
+            // higher_jumpbtn
+            // 
+            this.higher_jumpbtn.BackColor = System.Drawing.Color.Transparent;
+            this.higher_jumpbtn.Enabled = false;
+            this.higher_jumpbtn.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.higher_jumpbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.higher_jumpbtn.Font = new System.Drawing.Font("Segoe UI", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.higher_jumpbtn.ForeColor = System.Drawing.Color.Black;
+            this.higher_jumpbtn.Location = new System.Drawing.Point(3, 3);
+            this.higher_jumpbtn.Name = "higher_jumpbtn";
+            this.higher_jumpbtn.Size = new System.Drawing.Size(74, 35);
+            this.higher_jumpbtn.TabIndex = 2;
+            this.higher_jumpbtn.Text = "higher jump\r\n30bp";
+            this.higher_jumpbtn.UseVisualStyleBackColor = false;
+            this.higher_jumpbtn.Click += new System.EventHandler(this.higher_jumpbtn_Click);
+            // 
             // jump_btn
             // 
+            this.jump_btn.Enabled = false;
             this.jump_btn.FlatAppearance.BorderColor = System.Drawing.Color.White;
             this.jump_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.jump_btn.ForeColor = System.Drawing.Color.White;
-            this.jump_btn.Location = new System.Drawing.Point(359, 3);
+            this.jump_btn.Location = new System.Drawing.Point(694, 8);
             this.jump_btn.Name = "jump_btn";
             this.jump_btn.Size = new System.Drawing.Size(94, 38);
             this.jump_btn.TabIndex = 0;
             this.jump_btn.Text = "Jump";
             this.jump_btn.UseVisualStyleBackColor = true;
+            this.jump_btn.Visible = false;
             this.jump_btn.Click += new System.EventHandler(this.jump_btn_Click);
             // 
             // Mario
@@ -94,7 +126,6 @@
             // 
             // plant_timer
             // 
-            this.plant_timer.Enabled = true;
             this.plant_timer.Interval = 10;
             this.plant_timer.Tick += new System.EventHandler(this.plant_timer_Tick);
             // 
@@ -105,7 +136,6 @@
             // 
             // gravity
             // 
-            this.gravity.Enabled = true;
             this.gravity.Interval = 10;
             this.gravity.Tick += new System.EventHandler(this.gravity_Tick);
             // 
@@ -176,12 +206,26 @@
             this.pause.TabIndex = 8;
             this.pause.Text = "| |";
             // 
+            // startbtn
+            // 
+            this.startbtn.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.startbtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.startbtn.ForeColor = System.Drawing.Color.White;
+            this.startbtn.Location = new System.Drawing.Point(359, 198);
+            this.startbtn.Name = "startbtn";
+            this.startbtn.Size = new System.Drawing.Size(94, 38);
+            this.startbtn.TabIndex = 2;
+            this.startbtn.Text = "START";
+            this.startbtn.UseVisualStyleBackColor = true;
+            this.startbtn.Click += new System.EventHandler(this.startbtn_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.MediumTurquoise;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.startbtn);
             this.Controls.Add(this.pause);
             this.Controls.Add(this.bp);
             this.Controls.Add(this.buy_points);
@@ -195,6 +239,7 @@
             this.Text = "Form1";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             this.panel1.ResumeLayout(false);
+            this.buy_panel.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.Mario)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.Flower)).EndInit();
             this.ResumeLayout(false);
@@ -217,5 +262,8 @@
         private Label buy_points;
         private Label bp;
         private Label pause;
+        private Panel buy_panel;
+        private Button higher_jumpbtn;
+        private Button startbtn;
     }
 }
--- MarioGame/MarioGame/Form1.cs
+++ MarioGame/MarioGame/Form1.cs
@@ -10,8 +10,13 @@ namespace MarioGame
         {
             InitializeComponent();
             bp.Text = GetBP().ToString();
+            if(GetBP() >= 30)
+            {
+                higher_jumpbtn.Enabled = true;
+            }
         }
-        //plant thing
+        //plant functions
+        int speed = 100;
         private void plant_timer_Tick(object sender, EventArgs e)
         {
             bool game_over = 
@@ -31,26 +36,49 @@ namespace MarioGame
                 game_Over.Visible = true;
                 plant_timer.Enabled = false;
                 mario_timer.Enabled = false;
+                SaveScore();
             }
             else
             {
-                plant_default_location[0] -= 6;
+                plant_default_location[0] -= 6 * speed / 500;
+                if(speed <= 20000)
+                {
+                speed++;
+
+                }
                 Flower.Location = new Point(plant_default_location[0], plant_default_location[1]);
+            }
+        }
+        /// <summary>
+        /// Saves the player score
+        /// </summary>
+        private void SaveScore()
+        {
+            string[] conf = File.ReadAllLines(path);
+            int value = int.Parse(bp.Text) + score_points;
+            conf[0] = "coins=" + value;
+            using (var writer = new StreamWriter(path)) 
+            {
+                foreach(var line in conf)
+                {
+                    writer.WriteLine(line);
+                }
             }
         }
         //Jumping
         int jump_count = 0;
+        int jump_height = 45;
         private void mario_timer_Tick(object sender, EventArgs e)
         {
             if(jump_count < 3)
             {
-                mario_default_location[1] -= 45;
+                mario_default_location[1] -= jump_height;
                 Mario.Location = new Point(mario_default_location[0], mario_default_location[1]);
                 jump_count++;
             }
             else if(jump_count >= 3 && jump_count <=5)
             {
-                mario_default_location[1] -= 12;
+                mario_default_location[1] -= jump_height - 33;
                 Mario.Location = new Point(mario_default_location[0], mario_default_location[1]);
                 jump_count++;
             }
@@ -88,12 +116,33 @@ namespace MarioGame
                 mario_timer.Enabled = true;
             }
         }
+        //Jumping with space
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.KeyValue == (int)Keys.Space)
             {
                 mario_timer.Enabled = true;
             }
+        }
+
+        //from buy panel
+        private void higher_jumpbtn_Click(object sender, EventArgs e)
+        {
+            if(jump_height != 60)
+            {
+                bp.Text = (int.Parse(bp.Text) - 30).ToString();
+                jump_height = 60;
+            }
+        }
+
+        private void startbtn_Click(object sender, EventArgs e)
+        {
+            jump_btn.Visible = true;
+            jump_btn.Enabled = true;
+            gravity.Enabled = true;
+            plant_timer.Enabled = true;
+            startbtn.Enabled = false;
+            startbtn.Visible = false;
         }
     }
 }
